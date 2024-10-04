b8150ae3a164992b7cf7870dc4e7702f74a6e031
aima-iftikhar <emmaiftikhar3@gmail.com>
2024-10-04 09:02:43 -0700

    template

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+'use client';
+import React, { useState } from "react";
+import { Menu, MenuItem, } from "./ui/Navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+const [active, setActive] = useState<string | null>(null)
+    return (
+    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50")}>
+         <Menu setActive={setActive}>
+         <MenuItem setActive={setActive} active={active} item="Home">
+         <Link href="/"onClick={() => setActive("Home")}>Home</Link>
+         </MenuItem>
+         <MenuItem setActive={setActive} active={active} item="our courses">
+         <Link href="/courses"onClick={() => setActive("Our courses")}>Our courses</Link>
+         </MenuItem>
+         <MenuItem setActive={setActive} active={active} item="Contact">
+         <Link href="/contact"onClick={() => setActive("Contact")}>Contact</Link>
+         </MenuItem>
+         </Menu>
+    </div>
+  )
+}
+
+export default Navbar
